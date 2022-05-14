6b58f81fd0caf77e0b81c9c40518deda8d9adc62
Mateus Luiz <mateusluizz@hotmail.com>
2022-05-13 21:36:55 -0400

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,45 +1,56 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing(){
 
-axios.get(`${BASE_URL}/movies?size=12&page=0`)
+const [pageNumber, setPageNumer] = useState(0);
+
+useEffect(() => {
+  axios.get(`${BASE_URL}/movies?size=12&page=1`)
   .then(response => {
-    console.log(response.data);
-  })
-
-
-  return(
-    <>
-
-      <Pagination/>
-
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard />
-          </div>
-          
-          
-          
-        </div>
-      </div>
-
-      <MovieCard/>
-
-    </>
+    const data = response.data as MoviePage;
+    console.log(data);
+    setPageNumer(data.number);
     
+  });
+}, []);
+
+
+
+
+return(
+  <>
+  <p>{pageNumber}</p>
+    <Pagination/>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+        </div>
+        
+        
+        
+      </div>
+    </div>
+
+    <MovieCard/>
+
+  </>
+  
   )
 }
 
